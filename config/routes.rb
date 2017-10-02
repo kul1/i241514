795ecaacdde1f2e0295a109281b3373b723daae0
P Kul <1.0@kul.asia>
2017-10-02 12:50:29 -0500

    Init git

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  post ':controller(/:action(/:id))(.:format)'
+  get ':controller(/:action(/:id))(.:format)'
+  get '/logout' => 'sessions#destroy', :as => 'logout'
+  get '/auth/failure' => 'sessions#failure'
+  post '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/:provider/callback' => 'sessions#create'
+
+  resources :sessions
+  resources :identities
+  mount Ckeditor::Engine => '/ckeditor'
+  root :to => 'mindapp#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
