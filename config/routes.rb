d0198237847d5fa13cc85ed77ee9d1b5cbdfe382
P Kul <1.0@kul.asia>
2017-10-03 16:21:36 -0500

    Add Facebook share with kni-labs/rrssb

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :sessions
   resources :identities
   mount Ckeditor::Engine => '/ckeditor'
-  root :to => 'mindapp#index'
+  root :to => 'articles#my'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
